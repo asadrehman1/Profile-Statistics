d05cdba90252255dafc9860dc6495f2368d7fbe9
Asad Rehman <106298043+asadrehman1@users.noreply.github.com>
2022-08-12 02:45:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 const counters = document.querySelectorAll(".counter");
+let dataCeil=[];
 
-counters.forEach((elem)=>{
+//By using setInterval() Method
+for(let i=0;i<counters.length;i++){
     let count=0;
-    incr();
-    function incr(){
-    const dataCeil=elem.getAttribute("data-ceil")
-    count+=Math.ceil(dataCeil/15);
-    if(count<dataCeil){
-         elem.innerText=count;
-        setTimeout(incr,50)
+    console.log(count)
+
+    let interval=setInterval(()=>{
+        dataCeil[i]=counters[i].getAttribute("data-ceil");
+        count+=Math.ceil(dataCeil[i]/15)
+        if(count<dataCeil[i]){
+          counters[i].innerText=count;
+          console.log(counters[i].innerText);
         }
         else{
-        elem.innerText=dataCeil;
+          counters[i].innerText=dataCeil[i];
+          clearInterval(interval)
         }
-    }
-})
+      },50)
+
+   }
+   
+//-----------------------------------------------------------------------------------------------
+// By Using setTimeOut() Method
+
+// counters.forEach((elem)=>{
+//     let count=0;
+//     console.log(count)
+//     func();
+//     function func(){
+//     const dataCeil=elem.getAttribute("data-ceil")
+//     // console.log(dataCeil)
+//     count+=Math.ceil(dataCeil/15);
+//     console.log(elem)
+
+//     if(count < dataCeil){
+//         elem.innerText=count;
+//         setTimeout(func,1000)
+//         }
+//     else{
+//         elem.innerText=dataCeil;
+//         }
+//     };
+// })
